7a12c802fb27b1bfabe749107f5eac3137195263
Muhammad-Rubel <rubel.contact@gmail.com>
2023-03-11 11:07:21 +0600

    Project created

---
+++ svelte.config.js
@@ -0,0 +1,28 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/portfolio' : ''
+		}
+	},
+	paths: {
+		base: '/portfolio',
+		assets: '/portfolio'
+	}
+};
+
+export default config;
