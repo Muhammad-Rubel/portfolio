2e6f707f56ba42729c032ab0fbcbe20cd12286ea
Muhammad-Rubel <rubel.contact@gmail.com>
2023-03-11 12:52:51 +0600

    Added colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1 class="text-4xl font-bold font-fira text-lightest-navy">Welcome to SvelteKit</h1>
+<h1 class="text-4xl font-bold font-fira text-dark-navy">Welcome to dark-navy</h1>
+<h1 class="text-4xl font-bold font-fira text-navy">Welcome to navy</h1>
+<h1 class="text-4xl font-bold font-fira text-light-navy">Welcome to light-navy</h1>
+<h1 class="text-4xl font-bold font-fira text-lightest-navy">Welcome to lightest-navy</h1>
+<h1 class="text-4xl font-bold font-fira text-navy-shadow">Welcome to navy-shadow</h1>
+<h1 class="text-4xl font-bold font-fira text-slate">Welcome to slate</h1>
+<h1 class="text-4xl font-bold font-fira text-light-slate">Welcome to light-slate</h1>
+<h1 class="text-4xl font-bold font-fira text-lightest-slate">Welcome to lightest-slate</h1>
+<h1 class="text-4xl font-bold font-fira text-white">Welcome to White</h1>
+<h1 class="text-4xl font-bold font-fira text-green">Welcome to green</h1>
+<h1 class="text-4xl font-bold font-fira text-green-tint">Welcome to green-tint</h1>
+<h1 class="text-4xl font-bold font-fira text-pink">Welcome to Pink</h1>
+<h1 class="text-4xl font-bold font-fira text-blue">Welcome to blue</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
