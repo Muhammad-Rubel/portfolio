631548aad81ece1900955485880e6a8c7b781f24
Muhammad-Rubel <rubel.contact@gmail.com>
2025-01-25 15:04:44 +0600

    Updates

--- _app/immutable/chunks/1.22f18e4d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.c879c932.js";export{t as component};
--- _app/immutable/chunks/2.72a923a5.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.a337ce3b.js";export{t as component};
